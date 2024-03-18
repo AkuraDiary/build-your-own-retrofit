c8735ef2f53b6bc632db583a3c801cf37c45e5d6
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-03-19 04:57:04 +0700

    initial setup

---
+++ app/src/main/java/com/asthiseta/diyretrofit/model/RestaurantModel.kt
@@ -0,0 +1,10 @@
+package com.asthiseta.diyretrofit.model
+
+data class RestaurantModel(
+    var city: String?,
+    var description: String?,
+    var id: String?,
+    var name: String?,
+    var pictureId: String?,
+    var rating: Double?
+)
