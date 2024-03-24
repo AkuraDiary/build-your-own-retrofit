f3facc1c97fcca6230e21672c8c536771dc8ebf2
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-03-24 21:53:17 +0700

    mabuk parsing

--- app/src/main/java/com/asthiseta/diyretrofit/networking/parser/Parser.kt
+++ app/src/main/java/com/asthiseta/diyretrofit/networking/parser/Parser.kt
@@ -1,69 +1,54 @@
 package com.asthiseta.diyretrofit.networking.parser
 
-import android.util.Log
-import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
 import com.asthiseta.diyretrofit.networking.client.Client
 import org.json.JSONArray
 import org.json.JSONObject
 import java.lang.reflect.ParameterizedType
 
 interface Parser {
-    fun <T> parse(jsonString: String, clazz: Class<T>): T?
+    fun <T> parse(jsonString: String, clazz: Class<T>): Any?
 }
 
 
-@Suppress("UNCHECKED_CAST")
 class JsonParser : Parser {
-    override fun <T> parse(jsonString: String, clazz: Class<T>): T? {
-
-        val isRawList = clazz.isAssignableFrom(List::class.java)
-        val jsonObject =
-            if (isRawList) {
-
-                // If jsonString starts with '[', it indicates it's an array, so wrap it in a JSONObject
-                val jsonArray = JSONArray(jsonString)
-                JSONObject().apply { put("arrayData", jsonArray) }
-            } else {
-                JSONObject(jsonString)
-            }
-
-        @Suppress("IMPLICIT_CAST_TO_ANY")
-        val instance =
-            if (isRawList) ListHolder::class.java.newInstance() else clazz.newInstance()
-        val declaredFields =
-            if (isRawList) ListHolder::class.java.declaredFields else clazz.declaredFields
-
-        for (field in declaredFields) {
-            field.isAccessible = true
-            val fieldName = field.name
-            val fieldValue = jsonObject.opt(fieldName)
-            if (fieldValue != null) {
-                when (field.type) {
-                    List::class.java -> {
-
-                        // Handle list type
-                        val genericType = field.genericType as ParameterizedType
-                        val listType = genericType.actualTypeArguments[0] as Class<*>
-                        val itemList = parseList(fieldValue.toString(), listType)
-                        field.set(instance, itemList)
-                    }
-
-                    JSONObject::class.java -> {
-                        // Handle JSONObject type
-                        val item = parse(fieldValue.toString(), field.type)
-                        field.set(instance, item)
-                    }
-
-                    else -> {
-                        field.set(instance, fieldValue)
+    override fun <T> parse(jsonString: String, clazz: Class<T>): Any? {
+        try{
+            val jsonObject = JSONObject(jsonString)
+            val instance = clazz.newInstance()
+            val declaredFields = clazz.declaredFields
+            for (field in declaredFields) {
+                field.isAccessible = true
+                val fieldName = field.name
+                val fieldValue = jsonObject.opt(fieldName)
+                if (fieldValue != null) {
+                    when (field.type) {
+                        List::class.java -> {
+                            // Handle list type inside the object
+                            val genericType = field.genericType as ParameterizedType
+                            val listType = genericType.actualTypeArguments[0] as Class<*>
+                            val itemList = parseList(fieldValue.toString(), listType)
+                            field.set(instance, itemList)
+                        }
+                        JSONObject::class.java -> {
+                            // Handle JSONObject type
+                            val item = parse(fieldValue.toString(), field.type)
+                //                    Client.log("$fieldName: $item")
+                            field.set(instance, item)
+                        }
+                        else -> {
+                            // Handle other types
+                            field.set(instance, fieldValue)
+                        }
                     }
                 }
             }
+            return instance
+        } catch (e: Exception) {
+            Client.errorLog("Error parsing json: $jsonString")
+            Client.errorLog("Error message: ${e.message}")
+            e.printStackTrace()
+            return null
         }
-
-        val returnInstance =
-            if (isRawList) (instance as ListHolder<T>).arrayData else instance as T?
-        return returnInstance
     }
 
     private fun parseList(toString: String, listType: Class<*>): Any? {
@@ -76,4 +61,6 @@ class JsonParser : Parser {
         }
         return list
     }
+
+
 }
